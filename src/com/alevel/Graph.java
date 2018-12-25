7438d30c260ff9adfea5651b913edc1c8f0a80ac
SvdEdge <meruzhan.avetisian@gmail.com>
2018-12-26 00:44:52 +0200

    Initial commit

---
+++ src/com/alevel/Graph.java
@@ -0,0 +1,187 @@
+package com.alevel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Graph {
+    private static final int SOURCE_NODE_INDEX = 0;
+    private List<Node> nodes = new ArrayList<>();
+    private List<Edge> edges;
+    private int nodesNumber;
+    private int edgesNumber;
+    private int visitedNodesNumber = 0;
+
+    public Graph(List<Edge> edges) {
+        this.edges = edges;
+        this.edgesNumber = edges.size();
+        this.nodesNumber = this.getNodesNumber();
+        for (int i = 0; i < this.nodesNumber; i++) {
+            this.nodes.add(new Node());
+        }
+        for (int i = 0; i < this.edgesNumber; i++) {
+            this.nodes.get(this.edges.get(i).getFromNodeIndex()).addEdge(this.edges.get(i));
+            this.nodes.get(this.edges.get(i).getToNodeIndex()).addEdge(this.edges.get(i));
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder repr = new StringBuilder();
+        System.out.println("Number of nodes: " + nodesNumber);
+        for (Edge edge : edges) {
+            repr.append("It takes ")
+                    .append(edge.getLength())
+                    .append(" units from the node #")
+                    .append(edge.getFromNodeIndex())
+                    .append(" to the node #")
+                    .append(edge.getToNodeIndex())
+                    .append("\n");
+        }
+        return repr.toString();
+    }
+
+    private int getNodesNumber() {
+        int nodesNumber = 0;
+        for (Edge edge : edges) {
+            if (edge.getFromNodeIndex() > nodesNumber) {
+                nodesNumber = edge.getFromNodeIndex();
+            }
+            if (edge.getToNodeIndex() > nodesNumber) {
+                nodesNumber = edge.getToNodeIndex();
+            }
+        }
+        nodesNumber++;
+        return nodesNumber;
+    }
+
+    public void calculateShortestPathsFromSourceByDijkstraAlgo() {
+
+        int nextNodeIndex = SOURCE_NODE_INDEX;
+        nodes.get(nextNodeIndex).setDistanceFromSource(0);
+        // for (Node node : nodes) {
+
+        while (this.hasUnvisitedNodes()) {
+            Node nextNode = nodes.get(nextNodeIndex);
+            System.out.println("Current node:: " + nextNode);
+            ArrayList<Edge> edges = nextNode.getEdges();
+            for (Edge edge : edges) {
+                int neighborNodeIndex = edge.getNeighborNodeIndex(nextNodeIndex);
+                Node neighborNode = nodes.get(neighborNodeIndex);
+                if (!neighborNode.isVisited()) {
+                    int cost = nextNode.getDistanceFromSource() + edge.getLength();
+                    if (cost < neighborNode.getDistanceFromSource()) {
+                        neighborNode.setDistanceFromSource(cost);
+                        System.out.println("Updated neighbor node: " + neighborNode);
+                    }
+                }
+            }
+            nodes.get(nextNodeIndex).setVisited(true);
+            visitedNodesNumber++;
+            nextNodeIndex = getClosestAvailableNodeIndex();
+            System.out.println("------------------");
+        }
+
+
+    }
+
+
+    private int getClosestAvailableNodeIndex() {
+        int nodeIndex = 0;
+        int distance = Integer.MAX_VALUE;
+        for (int i = 0; i < nodesNumber; i++) {
+            int currentDistance = nodes.get(i).getDistanceFromSource();
+            if (!nodes.get(i).isVisited() && currentDistance < distance) {
+                distance = currentDistance;
+                nodeIndex = i;
+            }
+        }
+        return nodeIndex;
+    }
+
+
+    private List<Integer> populateItinerary(int nodeIndex,
+                                            int initialNodeIndex,
+                                            int totalDistance,
+                                            int minDistance,
+                                            List<Integer> itinerary) {
+
+        List<Integer> result = null;
+        if (totalDistance == minDistance) {
+            result = itinerary;
+        } else {
+            if (nodeIndex == SOURCE_NODE_INDEX) {
+                itinerary.clear();
+                totalDistance = 0;
+                populateItinerary(initialNodeIndex, initialNodeIndex, totalDistance, minDistance, itinerary);
+            } else {
+                Node node = nodes.get(nodeIndex);
+                ArrayList<Edge> edges = node.getEdges();
+                for (Edge edge : edges) {
+                    int neighborNodeIndex = edge.getNeighborNodeIndex(nodeIndex);
+                    totalDistance += nodes.get(neighborNodeIndex).getDistanceFromSource();
+                    nodeIndex = neighborNodeIndex;
+                    itinerary.add(nodeIndex);
+                    populateItinerary(nodeIndex, initialNodeIndex, totalDistance, minDistance, itinerary);
+                }
+            }
+        }
+        return result;
+    }
+
+    private void defineItinerary(int nodeIndex) {
+        if (this.hasUnvisitedNodes()) {
+            throw new IllegalStateException("All graph's nodes should have weights.");
+        }
+        ArrayList<Integer> itinerary = new ArrayList<>();
+        int minDistance = nodes.get(nodeIndex).getDistanceFromSource();
+        populateItinerary(nodeIndex, nodeIndex, 0, minDistance, itinerary);
+        nodes.get(nodeIndex).setPathFromSource(itinerary);
+    }
+
+    public StringBuilder getShortestPathFromSourceDescription() {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < nodesNumber; i++) {
+            output.append("\nThe shortest distance from the source node #0 to the node #")
+                    .append(i)
+                    .append(" is ")
+                    .append(nodes.get(i).getDistanceFromSource())
+                    .append(". The itinerary is: ")
+                    .append(nodes.get(i).getPathFromSource());
+        }
+        return output;
+    }
+
+    public boolean hasUnvisitedNodes() {
+        return visitedNodesNumber != nodesNumber;
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(ArrayList<Node> nodes) {
+        this.nodes = nodes;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public void setEdges(ArrayList<Edge> edges) {
+        this.edges = edges;
+    }
+
+    public void setNodesNumber(int nodesNumber) {
+        this.nodesNumber = nodesNumber;
+    }
+
+    public int getEdgesNumber() {
+        return edgesNumber;
+    }
+
+    public void setEdgesNumber(int edgesNumber) {
+        this.edgesNumber = edgesNumber;
+    }
+}
---
+++ src/com/alevel/Node.java
@@ -0,0 +1,73 @@
+package com.alevel;
+
+import java.util.ArrayList;
+
+public class Node {
+
+    private boolean visited;
+    private int distanceFromSource;
+
+    private ArrayList<Integer> pathFromSource;
+    private ArrayList<Edge> edges;
+
+    public Node() {
+        this.visited = false;
+        this.distanceFromSource = Integer.MAX_VALUE;
+        this.edges = new ArrayList<>();
+        this.pathFromSource = new ArrayList<>();
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public int getDistanceFromSource() {
+        return distanceFromSource;
+    }
+
+    public void setDistanceFromSource(int distanceFromSource) {
+        this.distanceFromSource = distanceFromSource;
+    }
+
+    public ArrayList<Integer> getPathFromSource() {
+        return pathFromSource;
+    }
+
+    public void setPathFromSource(ArrayList<Integer> pathFromSource) {
+        this.pathFromSource = pathFromSource;
+    }
+
+    public void addToPathFromSource(int nodeIndex) {
+        pathFromSource.add(nodeIndex);
+    }
+
+    public ArrayList<Edge> getEdges() {
+        return edges;
+    }
+
+    public void addEdge(Edge edge) {
+        this.edges.add(edge);
+    }
+
+    public void addPathEntry(int i) {
+        this.pathFromSource.add(i);
+    }
+
+    public void setEdges(ArrayList<Edge> edges) {
+        this.edges = edges;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "visited=" + visited +
+                ", distanceFromSource=" + distanceFromSource +
+                // ", pathFromSource=" + pathFromSource +
+                ", edges=" + edges +
+                '}';
+    }
+}
